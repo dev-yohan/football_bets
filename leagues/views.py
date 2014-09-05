1dc1ed57f00c21a5dba209f7dbf224122b51bd45
dev-yohan <yohanmorales@hotmail.com>
2014-09-04 19:29:20 -0500

    matches sidebar

--- leagues/views.py
+++ leagues/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 from teams.models import League
+from matches.models import Match
 
 #leagues index
 def index(request):
 
     leagues_list = League.objects.all()
-    context = {'leagues_list': leagues_list}
+    matches = Match.objects.order_by('match_date')[:50]
+    context = {'leagues_list': leagues_list, 'matches': matches}
     return render(request, 'leagues/index.html', context)
 
 #League detail
--- teams/views.py
+++ teams/views.py
@@ -7,12 +7,14 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 from teams.models import Team
+from matches.models import Match
 
 
 def index(request):
 
     team_list = Team.objects.all()
-    context = {'team_list': team_list}
+    matches = Match.objects.order_by('match_date')[:50]
+    context = {'team_list': team_list, 'matches': matches}
     return render(request, 'teams/index.html', context)
 
 def detail(request,  team_id, team_slug):
