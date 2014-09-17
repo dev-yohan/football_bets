8fa77302849792c64b68d0cddf9b21d0f5e4472d
yohan morales <yohanmorales@hotmail.com>
2014-09-16 23:14:51 -0500

    team detail

--- teams/views.py
+++ teams/views.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 # Create your views here.
 from django.http import HttpResponse
@@ -19,5 +20,6 @@ def index(request):
 def detail(request,  team_id, team_slug):
 
     team = get_object_or_404(Team,  pk=team_id)
-    return render(request, 'teams/detail.html', {'team': team})
+    matches = Match.objects.filter(Q(home=team) | Q(away=team), match_date__gte=datetime.datetime.today().date()).order_by('match_date')[:50]
+    return render(request, 'teams/detail.html', {'team': team,'matches': matches})
 
