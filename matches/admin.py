eacdcec913f2e58ee3d5d78a26a730db916f32d9
dev-yohan <yohanmorales@hotmail.com>
2014-09-22 16:38:57 -0500

    results forecast model

--- matches/admin.py
+++ matches/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from matches.models import Match, CrowdResult
+from matches.models import Match, CrowdResult, ResultForecast
 
 admin.site.register(Match)
 admin.site.register(CrowdResult)
+admin.site.register(ResultForecast)
