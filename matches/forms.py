a587039b75dc991cc665922115b8a2dd7730b469
yohan morales <yohanmorales@hotmail.com>
2014-09-22 22:11:56 -0500

    results forecast partial

--- matches/forms.py
+++ matches/forms.py
@@ -4,4 +4,9 @@ from models import CrowdResult
 class CrowdResultForm(forms.Form):
   home_goals = forms.DecimalField(max_digits=5, decimal_places=0)
   away_goals = forms.DecimalField(max_digits=5, decimal_places=0)
+  
+
+class ResultForecastForm(forms.Form):
+  home_goals = forms.DecimalField(max_digits=5, decimal_places=0, required=False)
+  away_goals = forms.DecimalField(max_digits=5, decimal_places=0, required=False)  
 
--- matches/views.py
+++ matches/views.py
@@ -1,11 +1,13 @@
 import datetime
-from forms import CrowdResultForm
+from forms import CrowdResultForm, ResultForecastForm
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.http import HttpResponse
-from matches.models import Match
+from matches.models import Match, CrowdResult, ResultForecast
+from django.db.models import Count
+from django import forms
 
 #Matches index
 def index(request):
@@ -19,26 +21,64 @@ def detail(request,  match_id, match_slug):
 
     match = get_object_or_404(Match,  pk=match_id)
     matches = Match.objects.filter(match_date__gte=datetime.datetime.today().date()).exclude(name=match.name).order_by('match_date')[:50]
+    result_forecasts = ResultForecast.objects.filter(match=match).order_by('-created_date')[:20]
     
-    
-    return render(request, 'matches/detail.html', {'match': match, 'matches': matches, 'user': request.user})
+    return render(request, 'matches/detail.html', 
+                  {'match': match, 
+                   'matches': matches, 
+                   'result_forecasts': result_forecasts,
+                   'user': request.user})
 
 #Match forecast new form
 def create_match_forecast(request, match_id, match_slug):
     match = get_object_or_404(Match,  pk=match_id)
     matches = Match.objects.filter(match_date__gte=datetime.datetime.today().date()).exclude(name=match.name).order_by('match_date')[:50]
     
-    return render(request, 'matches/create_match_forecast.html', {'match': match, 'matches': matches, 'user': request.user})
+    if request.user.is_authenticated():
+       
+       form = ResultForecastForm(request.POST) 
+       print form.errors
+       if request.method == 'POST'  and form.is_valid():
+       
+          home_goals = form.cleaned_data['home_goals']
+          away_goals = form.cleaned_data['away_goals']
+          
+          ResultForecast.objects.create(home_goals=home_goals, 
+                                        away_goals=away_goals,
+                                        user=request.user,
+                                        match=match,
+                                        created_date=datetime.datetime.now())
+          return render(request, 'matches/create_match_forecast.html', 
+                     {'match': match, 
+                      'matches': matches,
+                      'form': form, 
+                      'user': request.user})
+          
+       else:
+           
+         form = ResultForecastForm()
+            
+         return render(request, 'matches/create_match_forecast.html', 
+                     {'match': match, 
+                      'matches': matches,
+                      'form': form, 
+                      'user': request.user})
+    
+    else:
+        #messages.add_message(request, messages.INFO, 'Hello world.')
+        return redirect('matches:detail', match_id=match.id, match_slug=match.slug)
     
 #CrowdResult list
 def crowd_result_list(request, match_id):
     match = get_object_or_404(Match,  pk=match_id)
     matches = Match.objects.filter(match_date__gte=datetime.datetime.today().date()).exclude(name=match.name).order_by('match_date')[:50]
+    crowd_results = CrowdResult.objects.filter(match=match)
     
     return render(request, 'matches/crowd_result_list.html', 
                       {'match': match, 
                        'matches': matches,
-                       'user': request.user})
+                       'user': request.user,
+                       'crowd_results': crowd_results})
 
 #CrowdResult new form
 def create_crowd_result(request, match_id):
